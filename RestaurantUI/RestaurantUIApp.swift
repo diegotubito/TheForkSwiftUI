5ba84bfe1465419e9b0239f860bab466b67498ac
David Diego Gomez <diegodavid@icloud.com>
2022-11-13 20:18:29 -0300

    Initial Commit

---
+++ RestaurantUI/RestaurantUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantUIApp.swift
+//  RestaurantUI
+//
+//  Created by David Gomez on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
